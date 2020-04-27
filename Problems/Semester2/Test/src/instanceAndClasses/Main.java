5343d0ec419094a542809f2e5ee367a4ba56ee23
redrazer <39073078+redrazer@users.noreply.github.com>
2020-04-27 10:06:50 -0700

    Added a couple test files

---
+++ Problems/Semester2/Test/src/instanceAndClasses/Main.java
@@ -0,0 +1,10 @@
+package instanceAndClasses;
+
+public class Main {
+
+    public static void main(String[] args){
+        Bike myBike = new Bike(10, 27, "RED");
+        System.out.println(myBike.getBikeColor());
+    }
+
+}
