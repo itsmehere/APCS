4b7ffe0c409a9ae51821b0098c61dc46655e2d3e
redrazer <39073078+redrazer@users.noreply.github.com>
2020-04-27 16:16:04 -0700

    Added Sample AP problem - WordPair

---
+++ Problems/Semester2/WordPair/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numMatches = 0;
+        ArrayList<Wordpair> listOfWords = new ArrayList<Wordpair>();
+
+        listOfWords.add(new Wordpair("Apple", "Pie"));
+        listOfWords.add(new Wordpair("Orange", "Soda"));
+        listOfWords.add(new Wordpair("Cheese", "Cheese"));
+
+        for(int i = 0; i < listOfWords.size(); i++){
+            if(listOfWords.get(i).getFirstWord() == listOfWords.get(i).getSecondWord()){
+                numMatches++;
+            }
+        }
+
+        System.out.println(numMatches);
+    }
+
+}
